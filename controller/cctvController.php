bd3be6127b8b3560911a91ae99dfc43e60acfbe7
mike suarez <mike@gbpl.kr>
2025-05-15 15:46:10 +0800

    update cctv controller

--- controller/cctvController.php
+++ controller/cctvController.php
@@ -12,8 +12,19 @@ class CCTVController{
         return $this->table;
     } 
     public function create($input) {
-        $stmt = $this->conn->prepare("INSERT INTO $this->table (cctv_id, user_id) VALUES (?, ?)");
-        $stmt->bind_param("ss", $input['cctv_id'], $input['user_id']);
+        $stmt = $this->conn->prepare("INSERT INTO $this->table (cctv_id, user_id, ip_address, model, installation_location, isLive) VALUES (?, ?, ?, ?, ?, ?)");
+     
+        $stmt->bind_param(
+            "sssssi", 
+            $input['cctv_id'], 
+            $input['user_id'], 
+            $input['ip_address'], 
+            $input['model'], 
+            $input['installation_location'], 
+            $input['isLive'] 
+        );
+
+
         if ($stmt->execute()) {
             echo json_encode(["message" => "CCTV added", "id" => $stmt->insert_id]);
         } else {
@@ -22,8 +33,17 @@ class CCTVController{
     }
 
     public function update($id, $input) {
-        $stmt = $this->conn->prepare("UPDATE $this->table SET cctv_id = ?, user_id = ? WHERE id = ?");
-        $stmt->bind_param("ssi", $input['cctv_id'], $input['user_id'], $id);
+        $stmt = $this->conn->prepare("UPDATE $this->table SET cctv_id = ?, user_id = ?, ip_address = ?, model = ?, installation_location = ?, isLive = ? WHERE id = ?");
+         $stmt->bind_param(
+            "sssssi", 
+            $input['cctv_id'], 
+            $input['user_id'], 
+            $input['ip_address'], 
+            $input['model'], 
+            $input['installation_location'], 
+            $input['isLive'] 
+        );
+        // $stmt->bind_param("ssi", $input['cctv_id'], $input['user_id'], $id);
         if ($stmt->execute()) {
             echo json_encode(["message" => "CCTV updated"]);
         } else {
